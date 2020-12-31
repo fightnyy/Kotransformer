617e4ee217a45d3ea6a50ef38d648819bcd65ab1
fightnyy <fightnyy@naver.com>
2020-12-31 12:27:01 +0000

    name_chage

--- dataset/make_kowiki.py
+++ dataset/make_kowiki.py
@@ -4,21 +4,21 @@ import pandas as pd
 import datasetconfig
 import os
 import sys
+import csv
 
 scriptPATH = "../config/"
 sys.path.append(os.path.abspath(scriptPATH))
 import config
 
+csv.field_size_limit(sys.maxsize)
 logger = config.logger
 in_file = os.path.join(datasetconfig.sentencepath, "kowiki_20201230.csv")
 out_file = "kowiki.txt"
-seperator = u"u\241D"
-print("seperator : ",seperator)
+seperator = "␝"
 if os.path.exists(out_file):
     print("kowiki.txt is already exist")
 else :
     df =pd.read_csv(in_file, sep = seperator, engine="python")
-    print(df.head())
     with open(out_file, "w") as f:
         for index, row in df.iterrows():
             f.write(row["text"]) # title 과 text 가 중복되어 하나만 저장
