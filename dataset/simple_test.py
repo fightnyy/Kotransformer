c91629091be55162ffbee701b065fba762d17c99
fightnyy <fightnyy@naver.com>
2021-01-01 00:44:15 +0000

    Simple_test Addition

---
+++ dataset/simple_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sentencepiece as spm
+
+vocab_file = "kowiki.model"
+vocab = spm.SentencePieceProcessor()
+vocab.load(vocab_file)
+
+lines = ["안녕하세요 이제 세해가 되었네요", "저는 제가 좋아서 코딩을 합니다.","모두 행복한 새해 되세요!"]
+
+for line in lines :
+    pieces = vocab.EncodeAsPieces(line)
+    ids = vocab.encode_as_ids(line)
+    print(line) # 단순 문장
+    print(pieces) #잘라진 부분
+    print(ids)  # 그것들의 id
---
+++ dataset/train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sentencepiece as spm
+
+corpus = "kowiki.txt"
+prefix = "kowiki"
+vocab_size = 8000
+spm.SentencePieceTrainer.train(
+    f"--input={corpus} --model_prefix={prefix} --vocab_size={vocab_size+7}"+
+    " --model_type=bpe"+
+    " --max_sentence_length=999999"+
+    " --pad_id=0 --pad_piece=[PAD]"+
+    " --unk_id=1 --unk_piece=[UNK]"+
+    " --bos_id=2 --bos_piece=[BOS]"+
+    " --eos_id=3 --eos_piece=[EOS]"+
+    " --user_defined_symbols=[SEP],[CLS],[MASK]")
